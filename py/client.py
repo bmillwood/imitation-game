6f692b3232023efa58c53e3e6f3848ac7f5f90b6
Ben Millwood <thebenmachine+git@gmail.com>
2026-03-01 22:02:55 +0000

    defaults shenanigans

--- py/client.py
+++ py/client.py
@@ -8,23 +8,9 @@ import websockets.asyncio.client as wsc
 
 import protocol
 
-def createdAt_now():
-    return datetime.datetime.now(tz=datetime.timezone.utc).isoformat().replace("+00:00", "Z")
-
-async def send(ws, msg: dict):
-    msg["id"] = str(uuid.uuid4())
-    msg["createdAt"] = createdAt_now()
-    serialised = json.dumps(msg)
-    print(">>", serialised)
-    return await ws.send(serialised)
-
 async def main():
     async with wsc.connect("wss://www.rpm.cc/imitation/ws") as ws:
-        await ws.send(protocol.Ping(
-            id=protocol.Id(root=uuid.uuid4()),
-            createdAt=protocol.CreatedAt(root=datetime.datetime.now(tz=datetime.timezone.utc)),
-            type="ping",
-        ).model_dump_json())
+        await ws.send(protocol.Ping().model_dump_json())
         async for message in ws:
             print(json.loads(message))
 
--- py/generate_protocol.py
+++ py/generate_protocol.py
@@ -1,16 +1,49 @@
 #!/usr/bin/env python
 
-from pathlib import Path
+import contextlib
+import io
+import os
+import tempfile
 
 import datamodel_code_generator as dcg
 
 schema_path = "../packages/protocol/schema.json"
 
 json_schema = open(schema_path).read()
-result = dcg.generate(
-    input_=json_schema,
-    input_file_type=dcg.InputFileType.JsonSchema,
-    input_filename=schema_path,
-    output_model_type=dcg.DataModelType.PydanticV2BaseModel,
-    output=Path("protocol.py"),
-)
+
+buf = io.StringIO()
+with contextlib.redirect_stdout(buf):
+    dcg.generate(
+        input_=json_schema,
+        input_file_type=dcg.InputFileType.JsonSchema,
+        input_filename=schema_path,
+        output_model_type=dcg.DataModelType.PydanticV2BaseModel,
+        additional_imports=["datetime", "pydantic", "uuid"],
+    )
+
+preamble = """
+def createdAt_now() -> CreatedAt:
+    return CreatedAt(root=datetime.datetime.now(tz=datetime.timezone.utc))
+
+""".lstrip()
+
+code = buf.getvalue()
+with open("protocol.py", "w") as pf:
+    done_preamble = False
+    for line in code.splitlines():
+        if not done_preamble:
+            if line and not line.startswith("#") and not "import" in line:
+                pf.write(preamble)
+                done_preamble = True
+
+        match line:
+            case "    id: Id":
+                pf.write("    id: Id = pydantic.Field(default_factory=lambda: Id(root=uuid.uuid4()))\n")
+            case "    createdAt: CreatedAt":
+                pf.write("    createdAt: CreatedAt = pydantic.Field(default_factory=createdAt_now)\n")
+            case _:
+                if line.startswith("    type: Literal["):
+                    type_value = line.split("[", 1)[1].split("]", 1)[0]
+                    pf.write(f"{line} = {type_value}\n")
+                else:
+                    pf.write(f"{line}\n")
